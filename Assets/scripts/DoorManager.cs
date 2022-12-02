e1f39b76aedc3c76a5629d5593bd9f4ae537c288
Wrmorales <wrmorale@ucsc.edu>
2022-12-02 05:12:17 -0800

    added door opening and cursor change on doors

--- Assets/scripts/DoorManager.cs
+++ Assets/scripts/DoorManager.cs
@@ -8,30 +8,43 @@ public class DoorManager : MonoBehaviour
     public GameObject door;
     public bool orientation; //false left - true right 
     public Transform playerPos;
-    public Transform doorPos;
+    public Transform doorPivot;
     bool open = false;
 
 
     void OnMouseOver(){
-        float dist = Vector3.Distance(playerPos.position, doorPos.position);
+        float dist = Vector3.Distance(playerPos.position, doorPivot.position);
         Debug.Log("Distance to obj: "  + dist);
         if(dist <= 25f && open == false){
             Debug.Log("Mouse is over GameObject.");
 
                 if(Input.GetMouseButton(0)){
                     Debug.Log("Mouse Down");
-                    Vector3 pivotPoint = doorPos.position;
-                    transform.RotateAround(pivotPoint, Vector3.up, 90);
-                    open = true;
+                    Vector3 pivotPoint = doorPivot.position;
+                    if(orientation == false){
+                        transform.RotateAround(pivotPoint, Vector3.up, 90);
+                        open = true;
+                    }else if(orientation == true){
+                        transform.RotateAround(pivotPoint, Vector3.up, 270);
+                        open = true;
+                    }
+                    
                 }
         }else if(dist <= 25f && open == true){
             Debug.Log("Mouse is over GameObject.");
 
                 if(Input.GetMouseButton(0)){
                     Debug.Log("Mouse Down");
-                    Vector3 pivotPoint = doorPos.position;
-                    transform.RotateAround(pivotPoint, Vector3.up, -90);
-                    open = false;
+                    Vector3 pivotPoint = doorPivot.position;
+                    if(orientation == false){
+                        transform.RotateAround(pivotPoint, Vector3.up, -90);
+                        open = false;
+                    }
+                    if(orientation == true){
+                        transform.RotateAround(pivotPoint, Vector3.up, -270);
+                        open = true;
+                    }
+                    
                 }
         }
     }
